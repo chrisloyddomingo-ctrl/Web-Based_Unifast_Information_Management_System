5a260cb7c539fd4366824c97cd9e83e9610cee53
Chris Loyd Domingo <chrisloyd97@gmail.com>
2026-04-21 10:36:29 +0800

    fix: remove year_level dependency

--- database/migrations/2026_03_28_205413_add_new_fields_to_tblapplications_table.php
+++ database/migrations/2026_03_28_205413_add_new_fields_to_tblapplications_table.php
@@ -6,21 +6,18 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration
 {
-    /**
-     * Run the migrations.
-     */
-public function up(): void
-{
-    Schema::table('tblapplications', function (Blueprint $table) {
-        $table->boolean('first_generation')->after('year_level');
-        $table->string('parents_monthly_income')->after('first_generation');
-    });
-}
+    public function up(): void
+    {
+        Schema::table('tblapplications', function (Blueprint $table) {
+            $table->boolean('first_generation')->default(false);
+            $table->string('parents_monthly_income')->nullable();
+        });
+    }
 
-public function down(): void
-{
-    Schema::table('tblapplications', function (Blueprint $table) {
-        $table->dropColumn(['first_generation', 'parents_monthly_income']);
-    });
-}
+    public function down(): void
+    {
+        Schema::table('tblapplications', function (Blueprint $table) {
+            $table->dropColumn(['first_generation', 'parents_monthly_income']);
+        });
+    }
 };
